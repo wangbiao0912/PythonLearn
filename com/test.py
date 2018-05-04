5a6df353789ba39843a547dcaffc1d628a325d61
wangbiaoJava <229492022@qq.com>
2018-05-04 11:50:28 +0800

    'pyhon简单语法'

---
+++ com/test.py
@@ -0,0 +1,34 @@
+# -*- coding: UTF-8 -*-
+print('testfdsfdsf');
+if 1 == 1:
+    print("Answer")
+    print("True")
+else:
+    print("Anssadsadsawer")
+    # 没有严格缩进，在执行时会报错
+    print("False");
+    """"
+        多行注释
+    """
+    '''
+        多行注释
+    '''
+
+
+import sys; x = 'runoob'; sys.stdout.write(x)
+
+#raw_input("按下 enter 键退出，其他任意键显示..") #的是非得失
+
+x = "a"
+y = "b"
+# 换行输出
+print(x)
+print(y)
+
+print('---------')
+# 不换行输出
+print(x),
+print(y),
+
+# 不换行输出
+print(x, y);
